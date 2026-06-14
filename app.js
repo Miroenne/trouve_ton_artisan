23bafb0d644b169a530fd27e0d538065c8144c35
Math <mmenin8306@icloud.com>
2026-06-14 18:41:52 +0200

    Changed cors settings to allow localhost to use the API

--- app.js
+++ app.js
@@ -18,8 +18,19 @@ var app = express();
 DB.initDb();
 
 app.use(cors({
-    origin: '',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    exposedHeaders: ["Authorization"],
+    origin : (origin, callback) => {
+
+        /*const allowedOrigin = process.env.ALLOWED_ORIGIN;*/
+        const localRegex = /^http:\/\/(localhost|127\.0\.0\.1):\d+$/;
+        /*console.log(ALLOWED_ORIGIN)*/
+
+        if(!origin || localRegex.test(origin) /*|| origin === allowedOrigin*/){
+            return callback(null, true);
+        }
+
+        return callback(new Error('Origin non authorisée par CORS'));
+    },
     credentials: true
 }));
 
--- repositories/top3Repository.js
+++ repositories/top3Repository.js
@@ -13,7 +13,7 @@ exports.get = async () => {
     const filePath = path.join(__dirname, '../db/scripts/top_3.sql')    
     const sql = await fs.readFile(filePath, 'utf-8');            
     await connexion.changeUser({database: process.env.DATABASE})            
-    const top = await connexion.query(sql);    
+    const [top] = await connexion.query(sql);    
     connexion.end();
     return top;        
     
